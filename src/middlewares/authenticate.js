31b4399f32b3506cc1a37f2e9861d533e3ada543
uty <fgmapam@gmail.com>
2025-10-01 22:25:38 +0300

    Summary(required)

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -3,20 +3,24 @@ import { Session } from '../models/session.js';
 import { User } from '../models/user.js';
 
 export async function authenticate(req, _res, next) {
-  const hdr = req.get('Authorization') || '';
-  const [type, token] = hdr.split(' ');
-  if (type !== 'Bearer' || !token) throw new createHttpError.Unauthorized('Not authorized');
+  try {
+    const header = req.get('Authorization') || req.get('authorization') || '';
+    const [scheme, token] = header.split(' ');
+    if (scheme !== 'Bearer' || !token) {
+      return next(new createHttpError.Unauthorized('Not authorized'));
+    }
+    const session = await Session.findOne({ accessToken: token });
+    if (!session || session.accessTokenValidUntil < new Date) {
+      return next(new createHttpError.Unauthorized('Access token has expired'));
 
-  const session = await Session.findOne({ accessToken: token });
-  if (!session || session.accessTokenValidUntil < new Date()) {
-    throw new createHttpError.Unauthorized('Access token has expired');
+    }
+const user = await User.findById(session.userId).lean();
+    if (!user) {
+      return next(new createHttpError.Unauthorized('User not found'));
+    }
+    req.user = user;
+    return next();
+  } catch (error) {
+    return next(error);
   }
-
-  const user = await User.findById(session.userId).lean();
-  if (!user) throw new createHttpError.Unauthorized('Not authorized');
-
-  req.user = user;
-  next();
 }
-
-
